6e6c434a3b823f02b655099361c1a3265086f7f9
Burak <burak@llinformatics.com>
2021-05-13 15:18:57 +0200

    Change plugged authenticate/2 function name to authenticate_user/2

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -4,7 +4,7 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
 
   def index(conn, _params) do
     users = Accounts.list_users()
