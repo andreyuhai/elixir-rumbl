f55ac3c3c89ef049bf4ff111da08f29467645e1e
Burak <burak@llinformatics.com>
2021-05-13 15:18:19 +0200

    |Move authenticate/2 function here making it public and changing the name to authenticate_user/2

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -25,4 +25,15 @@ defmodule RumblWeb.Auth do
     # you could delete only the user.id  doing
     # delete_session(conn, :user_id)
   end
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
+  end
 end
