b0f671d138f6698d1b9d378a24bd65647dd32c4e
Burak <burak@llinformatics.com>
2021-05-14 09:43:39 +0200

    Permit :category_id in params

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -16,7 +16,7 @@ defmodule Rumbl.Multimedia.Video do
   @doc false
   def changeset(video, attrs) do
     video
-    |> cast(attrs, [:url, :title, :description])
+    |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
   end
 end
