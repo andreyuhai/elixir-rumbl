415b23e0858c0af8562b623bac3ced5ec9ec8206
Burak <burak@llinformatics.com>
2021-05-18 09:46:52 +0200

    Implement connect/3 function using token param and verify the token inside the function

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -15,10 +15,23 @@ defmodule RumblWeb.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
+  @max_age 2 * 7 * 24 * 60 * 60
   @impl true
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case Phoenix.Token.verify(
+      socket,
+      "user socket",
+      token,
+      max_age: @max_age
+    ) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
+
+  def connect(_params, _socket, _connect_info), do: :error
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
