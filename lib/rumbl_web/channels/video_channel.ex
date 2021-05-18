be9e37eeb30770ce7eda5a7c5fb11efaa074c455
Burak <burak@llinformatics.com>
2021-05-18 14:09:23 +0200

    Implement new handle_in/3 and handle_in/4 functions

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -1,26 +1,29 @@
 defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
+  alias Rumbl.{Accounts, Multimedia}
 
   def join("videos:" <> video_id, _params, socket) do
-    # :timer.send_interval(5_000, :ping)
-    # {:ok, assign(socket, :video_id, String.to_integer(video_id))}
-    {:ok, socket}
+    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
   end
 
-  # def handle_info(:ping, socket) do
-  #   count = socket.assigns[:count] || 1
-  #   push(socket, "ping", %{count: count})
+  def handle_in(event, params, socket) do
+    user = Accounts.get_user!(socket.assigns.user_id)
+    handle_in(event, params, user, socket)
+  end
 
-  #   {:noreply, assign(socket, :count, count + 1)}
-  # end
+  def handle_in("new_annotation", params, user, socket) do
+    case Multimedia.annotate_video(user, socket.assigns.video_id, params) do
+      {:ok, annotation} ->
+        broadcast!(socket, "new_annotation", %{
+          id: annotation.id,
+          user: RumblWeb.UserView.render("user.json", %{user: user}),
+          body: annotation.body,
+          at: annotation.at
+        })
+        {:reply, :ok, socket}
 
-  def handle_in("new_annotation", params, socket) do
-    broadcast!(socket, "new_annotation", %{
-      user: %{username: "anon"},
-      body: params["body"],
-      at: params["at"]
-    })
-
-    {:reply, :ok, socket}
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
