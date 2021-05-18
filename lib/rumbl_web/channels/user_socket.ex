5985d9efd728baaa974b60f72ee887bfd9f5d304
Burak <burak@llinformatics.com>
2021-05-18 09:42:31 +0200

    Fix alignment of the line

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule RumblWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-channel "videos:*", RumblWeb.VideoChannel
+  channel "videos:*", RumblWeb.VideoChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
