8f36736c3f17658769266bb4727b35624c7c2cb4
Burak <burak@llinformatics.com>
2021-05-18 14:41:19 +0200

    Change join/3 to send back annotations in response

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -1,9 +1,18 @@
 defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
   alias Rumbl.{Accounts, Multimedia}
+  alias RumblWeb.AnnotationView
 
   def join("videos:" <> video_id, _params, socket) do
-    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+    video_id = String.to_integer(video_id)
+    video = Multimedia.get_video!(video_id)
+
+    annotations =
+      video
+      |> Multimedia.list_annotations()
+      |> Phoenix.View.render_many(AnnotationView, "annotation.json")
+
+    {:ok, %{annotations: annotations}, assign(socket, :video_id, video_id)}
   end
 
   def handle_in(event, params, socket) do
